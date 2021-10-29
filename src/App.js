c1b79ba9e842da18dd6ee6bc220be6749584c01a
Nickson Kaigi <nickaigi@gmail.com>
2021-10-29 13:10:18 +0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from './components/Header'
+
+
 function App() {
-  return (
-    <div className="App">
-        <h1> Hello From React</h1>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <Header title='Hello' />
+        </div>
+    );
 }
 
-export default App;
+export default App
