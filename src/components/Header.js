e229672d898421f0cdbcb5c10802f1105ab97151
Nickson Kaigi <nickaigi@gmail.com>
2021-10-29 13:17:10 +0300

    using default props, and passing in the title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-const Header = (props) => {
+const Header = ({title}) => {
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
 }
 
 export default Header
